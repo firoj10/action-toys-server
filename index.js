89744d3a107fe483100b546b2f537b68cb9fc052
firoj10 <mdfirojhasan.info@gmail.com>
2023-05-18 12:40:19 +0600

    toy details route  added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 
@@ -21,16 +21,47 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-app.post('/addToy', async(req, res)=>{
-    const addToy = req.body;
-    console.log(addToy);
-  
-  })
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db('actionToyDB').collection('actionToy');
+
+
+
+    app.get('/actionToy', async(req, res)=>{
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+      
+      })
+     ;
+      app.get('/actionToy/:id', async(req, res)=>{
+        const id = req.params.id;
+       const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(query);
+        res.send(result)
+      })
+      // app.get("/actionToy/:email", async (req, res) => {
+      //   console.log(req.params.id);
+      //   const jobs = await toyCollection
+      //     .find({
+      //       selleremail: req.params.email,
+      //     })
+      //     .toArray();
+      //   res.send(jobs);
+      
+      // });
+
+    app.post('/actionToy', async(req, res)=>{
+        const newToy = req.body;
+        console.log(newToy);
+        const result = await toyCollection.insertOne(newToy);
+        res.send(result)
+      
+      })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
